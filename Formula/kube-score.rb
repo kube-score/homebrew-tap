7420c4fd72d7eaa3cd998ff574f414f6ccbaec8a
goreleaserbot <goreleaser@carlosbecker.com>
2020-08-10 21:31:09 +0200

    Brew formula update for kube-score version v1.8.0

--- Formula/kube-score.rb
+++ Formula/kube-score.rb
@@ -2,16 +2,25 @@
 class KubeScore < Formula
   desc ""
   homepage "https://kube-score.com/"
-  version "1.7.3"
+  version "1.8.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/zegl/kube-score/releases/download/v1.7.3/kube-score_1.7.3_darwin_amd64.tar.gz"
-    sha256 "a02ba6a80e5b55b834e5f044a82502aa8a6a21a3412b7254cea50ed007ec1b65"
+    url "https://github.com/zegl/kube-score/releases/download/v1.8.0/kube-score_1.8.0_darwin_amd64.tar.gz"
+    sha256 "d81f40c5507716370173de4bc35b001928cba33d151c59e6ee24c11d09f8a051"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/zegl/kube-score/releases/download/v1.7.3/kube-score_1.7.3_linux_amd64.tar.gz"
-      sha256 "33192ba14973cc0a1b2dafacb31add6641eaa59eafb3a27da8eeb3fa2718ccfe"
+      url "https://github.com/zegl/kube-score/releases/download/v1.8.0/kube-score_1.8.0_linux_amd64.tar.gz"
+      sha256 "cb4464540ee87a27970aaf54a9b7b43ba20c617ddee7e693cb5694249faeb5df"
+    end
+    if Hardware::CPU.arm?
+      if Hardware::CPU.is_64_bit?
+        url "https://github.com/zegl/kube-score/releases/download/v1.8.0/kube-score_1.8.0_linux_arm64.tar.gz"
+        sha256 "0cadc65ae49c3a0c04c813e00f17fee31d38b826d44a764839e0d1514950c569"
+      else
+        url "https://github.com/zegl/kube-score/releases/download/v1.8.0/kube-score_1.8.0_linux_armv6.tar.gz"
+        sha256 "8fb533d3ae32e851347400016e4851350e018a02690389fb8cef87f368f1c702"
+      end
     end
   end
 
